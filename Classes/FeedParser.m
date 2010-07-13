c2edd927298b5aa87909890ca716fb87930298ea
phoet <phoetmail@googlemail.com>
2010-07-13 18:06:16 +0200

    fix category and more memory cleanup

--- Classes/FeedParser.m
+++ Classes/FeedParser.m
@@ -12,29 +12,33 @@
 
 @implementation FeedParser
 
-+ (NSString*) valueWithName:(NSString *) name fromElement:(TBXMLElement *) element  {
-	TBXMLElement* titleElement = [TBXML childElementNamed:@"title" parentElement:element];
-	NSString * title = [TBXML textForElement:titleElement];
-	title = [title stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-	NSLog(@"element has title %@", title);
-	return title;
++ (NSString*) valueWithName:(NSString *) name fromElement:(TBXMLElement*) element  {
+	TBXMLElement* childElement = [TBXML childElementNamed:name parentElement:element];
+	NSString* text = [[TBXML textForElement:childElement] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	NSLog(@"element %@ has text %@", name, text);
+	return text;
 }
 
 + (NSArray*) parseItemsFromURL:(NSString *) urlString{
-	TBXML * tbxml = [[TBXML tbxmlWithURL:[NSURL URLWithString:urlString]] retain];
-	// TODO check for error and report it back
-	TBXMLElement * rootXMLElement = tbxml.rootXMLElement;
-	NSString * name = [TBXML elementName:rootXMLElement];
-	NSLog(@"element has name %@", name);
-	TBXMLElement* element = [TBXML childElementNamed:@"item" parentElement:[TBXML childElementNamed:@"channel" parentElement:rootXMLElement]];
-	NSMutableArray * array = [[[NSMutableArray alloc] init] autorelease];
-	do {
-		Item * item = [[[Item alloc] init] autorelease];
-		item.title = [self valueWithName:@"title" fromElement:element];
-		item.category = [self valueWithName:@"category" fromElement:element];
-		[array addObject:item];
-		
-	} while ((element = element->nextSibling));
+	TBXML* tbxml = [[TBXML tbxmlWithURL:[NSURL URLWithString:urlString]] retain];
+	TBXMLElement* rootXMLElement = tbxml.rootXMLElement;
+	NSMutableArray* array = [[[NSMutableArray alloc] init] autorelease];
+	if (rootXMLElement) {
+		NSString* name = [TBXML elementName:rootXMLElement];
+		NSLog(@"element has name %@", name);
+		TBXMLElement* element = [TBXML childElementNamed:@"item" parentElement:[TBXML childElementNamed:@"channel" parentElement:rootXMLElement]];
+		name = [TBXML elementName:element];
+		NSLog(@"element has name %@", name);
+		do {
+			Item* item = [[[Item alloc] init] autorelease];
+			item.title = [self valueWithName:@"title" fromElement:element];
+			item.category = [self valueWithName:@"category" fromElement:element];
+			[array addObject:item];
+			
+		} while ((element = element->nextSibling));
+	}		
+	[tbxml release];
 	return array;
 }
+
 @end
--- Classes/dzoneAppDelegate.m
+++ Classes/dzoneAppDelegate.m
@@ -11,6 +11,8 @@
 #import "FeedParser.h"
 #import "Item.h"
 
+#define DZONE_URL @"http://feeds.dzone.com/dzone/frontpage"
+
 @implementation dzoneAppDelegate
 
 @synthesize window;
@@ -18,11 +20,20 @@
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {	
+	// TODO check why this can't be done in the viewController init method
 	viewController.listData = [[[NSMutableArray alloc] init] autorelease];
 	
-	NSArray * items = [FeedParser parseItemsFromURL: @"http://feeds.dzone.com/dzone/frontpage"];
-	for (Item * item in items) {
-		[viewController.listData addObject:item];
+	NSArray * items = [FeedParser parseItemsFromURL: DZONE_URL];
+	if ([items count] > 0 ) {
+		for (Item * item in items) {
+			[viewController.listData addObject:item];
+		}
+	} else {
+		NSString* errorMessage = [NSString stringWithFormat:@"Check your networking configuration, could not load %@", DZONE_URL];
+		UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"An Error Occured" message:errorMessage delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+		
+		[alertView show];
+		[alertView release];
 	}
 
     // Override point for customization after app launch    
